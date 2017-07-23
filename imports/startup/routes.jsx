37a5b2e178e8797b687b54b7fd079c0e6b64b108
Servio Mora <servio.mora19@gmail.com>
2017-07-23 15:38:56 -0400

    tested out new buttons

--- imports/startup/routes.jsx
+++ imports/startup/routes.jsx
@@ -4,7 +4,7 @@ import { mount } from 'react-mounter';
 
 //Views import
 import { App } from '../ui/layouts/App.jsx';
-import { Home } from '../ui/views/Home.jsx';
+import Home from '../ui/views/Home.jsx';
 
 
 FlowRouter.route('/', {
--- imports/ui/views/Home.jsx
+++ imports/ui/views/Home.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from '../../ui/components/nav';
 import SideNav from '../../ui/components/sideNav';
 import Slider from '../../ui/components/slider';
 import Column from '../../ui/components/column';
 
-export const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <SideNav />
-      <div id="home-main">
-        <Slider />
-        <Column />
+
+class Home extends Component {
+  componentDidMount() {
+    $(".button-fill").hover(function () {
+       $(this).children(".button-inside").addClass('full');
+    }, function() {
+       $(this).children(".button-inside").removeClass('full');
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <SideNav />
+        <div id="buttons">
+          <a href="#">
+            <div className="button-fill">
+              <div className="button-text">Hello</div>
+              <div className="button-inside">
+                <div className="inside-text">LOL</div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div id="home-main">
+          <Slider />
+          <Column />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+
+export default Home;
